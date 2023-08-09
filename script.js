722db1bc8baf225859cbac861e041fbde736dad8
Aishah Ahmeed <aishahahmeed@gmail.com>
2023-08-08 20:48:04 -0600

    Declare winner function

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 'use strict'
 
+const container = document.querySelector('.cell_container');
+
 
 const gameBoard = (() => {
-    let gameWin = false;
     let gameDraw = false;
     const BoardCell = (value, marked, empty) => {
         value,
-        marked, 
+        marked,
         empty = true;
-        return { value, empty } // maybe return marked if needed
+        return { value, empty, marked } // maybe return marked if needed
     };
 
     let gameBoardState = []
-    const container = document.querySelector('.cell_container');
+    
 
     function addToBoard() {
         for (let i = 0; i < 9; i++) {
@@ -28,63 +29,56 @@ const gameBoard = (() => {
     }
     addToBoard();
 
-    const checkForWin = () => {
-    let position = gameBoardState;
-
-        // diagonal
-        (() => {
-            if (
-                ( (position[0].marked === position[4].marked) && (position[4].marked === position[8].marked) ) ||
-                ( (position[2].marked === position[4].marked) &&  (position[4].marked === position[6].marked) )
-                ) {
-                    gameWin = true;
-                    console.log('diagonal win')
-                    console.log(gameWin);
-                    return gameWin;
-                }
-            }
-        )();
-
-        // horizontal
-        (() => {
-            if (
-                ( (position[0].marked === position[1].marked) && (position[1].marked ===  position[2].marked) ) ||
-                ( (position[3].marked === position[4].marked) && (position[4].marked ===  position[5].marked) ) ||
-                ( (position[6].marked === position[7].marked) && (position[7].marked ===  position[8].marked) )
-             )
-              {
-                gameWin = true;
-                console.log('horizontal win')
-                return gameWin;
-            }
-        })
-
-        // vertical
-        (() => {
-            if (
-                (position[0].marked === position[3].marked) && (position[3].marked === position[6].marked ) ||
-                (position[1].marked === position[4].marked) && (position[4].marked === position[7].marked ) ||
-                (position[2].marked === position[5].marked) && (position[5].marked === position[8].marked )
-            ) {
-                gameWin = true;
-                console.log('vertical win')
-                return gameWin;
-            }
-        })
-        return gameWin;
+    function checkForWin() {
+    let gameWin = 'false';
+    // horizontal
+    if (
+        (gameBoardState[0].marked === gameBoardState[1].marked && gameBoardState[1].marked === gameBoardState[2].marked && gameBoardState[1].marked !== undefined) ||
+        (gameBoardState[3].marked === gameBoardState[4].marked && gameBoardState[4].marked === gameBoardState[5].marked && gameBoardState[4].marked !== undefined) ||
+        (gameBoardState[6].marked === gameBoardState[7].marked && gameBoardState[7].marked === gameBoardState[8].marked && gameBoardState[7].marked !== undefined)
+        ) {
+        console.log('horizontal win!');
+        gameWin = true;
     }
+
+    // diagonal
+    if (gameBoardState[4].marked !== undefined) {
+        if (
+            (gameBoardState[0].marked === gameBoardState[4].marked && gameBoardState[4].marked === gameBoardState[8].marked) ||
+            (gameBoardState[2].marked === gameBoardState[4].marked && gameBoardState[4].marked === gameBoardState[6].marked)
+        ) {
+            console.log('diagonal win!');
+            gameWin = true;
+        }
+    }
+
+    // vertical 
+    if (
+        (gameBoardState[0].marked === gameBoardState[3].marked && gameBoardState[3].marked === gameBoardState[6].marked && gameBoardState[3].marked !== undefined) ||
+        (gameBoardState[1].marked === gameBoardState[4].marked && gameBoardState[4].marked === gameBoardState[7].marked && gameBoardState[4].marked !== undefined) ||
+        (gameBoardState[2].marked === gameBoardState[5].marked && gameBoardState[5].marked === gameBoardState[8].marked && gameBoardState[5].marked !== undefined) 
+    ) {
+        console.log('vertical win');
+        gameWin = true;
+    }
+
+    console.log('gameWin inside func is ' + gameWin)
+    return gameWin
+    }
+
+    checkForWin();
     
+    // console.log ('gameWin outisde func is ' + gameWin);
 
     return { 
         gameBoardState,
         container,
-        checkForWin,
+        checkForWin
     }
 
 })();
 
-// maybe delete marked inside playAMove? - may not need
-const Player = (name, marker, playerType) => {
+const Player = (name, marker) => {
     name
     const playAMove = (
         cell, // correspond to DOM div ID
@@ -98,16 +92,16 @@ const Player = (name, marker, playerType) => {
             board[cell].marked = activePlayer.marker;
             gameFlow.setActivePlayer().switchPlayer();
         } else alert('Please choose another spot!');
-        // gameBoard.checkForWin();
         gameBoard.checkForWin();
-       console.log('checked for win');  
+       console.log('checked for win');
+       // gamePlay.checkGameOver();
+       gameFlow.checkGameOver();
        console.log(board);  
     }; 
 
     return { 
         name, 
-        marker, 
-        // playerType,
+        marker,
         playAMove
     };
 }
@@ -117,7 +111,6 @@ const gameFlow = (() => {
     let gameEnd = false;
 
     const playerOne = Player('Player One', 'X');
-    // set playerTwo type as person || computer
     const playerTwo = Player('Player Two', 'O');
     
     const players = [ playerOne, playerTwo ];
@@ -130,7 +123,7 @@ const gameFlow = (() => {
             console.log(activePlayer)
         }
         // switchPlayer();
-
+        
         return {
             switchPlayer,
             activePlayer
@@ -138,23 +131,63 @@ const gameFlow = (() => {
         
     }
 
-    if (gameBoard.gameWin === true) {
-        // declareWinner();
-        // endGame();
-        gameEnd = true;
-        console.log('game win is TRUE!')
-    } /* else if (gameDraw === true) {
-        console.log('it\'s a tie!');
-        text.innerText = 'It\s a tie!'
-        text.innerText = 'Tie!'
-        gameEnd = true;
+    const checkGameOver = () => {
+        console.log(gameBoard.checkForWin());
+        console.log('that was win');
+        if (gameBoard.checkForWin() === true) {
+            declareWinner();
+            // endGame();
+            console.log('hey people');
+            gameEnd = true;
+            console.log('game win is TRUE!');
+            console.log('active player after WIN is: ' + activePlayer.name)
+            return gameEnd;
+    } else console.log('nope')
     }
-    */
+
+    const declareWinner = () => {
+        winner = activePlayer.name = 'Player Two' ? 'Player One' : 'Player Two';
+        console.log('The winner is ' + winner);
+        return winner;
+    }
 
     // reset board function
     return {
         playerOne,
         playerTwo,
         setActivePlayer,
+        checkGameOver
     }
 })();
+
+
+const displayController = (() => {
+    const text = document.createElement('div')
+    text.setAttribute('id', 'prompt');
+    text.innerText = 'Player One, please place your marker';
+    board.insertBefore(text, container);
+    
+})();
+
+
+
+/*
+const checkGameOver = () => {
+    if (gameBoard.gameWin === true) {
+        // declareWinner();
+        // endGame();
+        console.log('hey people');
+        console.log(gameWin);
+        gameEnd = true;
+        console.log('game win is TRUE!')
+        return gameEnd;
+        } /* else if (gameDraw === true) {
+        console.log('it\'s a tie!');
+        text.innerText = 'It\s a tie!'
+        text.innerText = 'Tie!'
+        gameEnd = true;
+        
+    // } 
+    return { checkGameOver }  
+}
+*/
